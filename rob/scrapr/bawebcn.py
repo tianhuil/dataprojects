0d3fefb306c05988a1c6bba313160fd5dca645c0
robhowley <howley.robert@gmail.com>
2014-06-11 00:23:39 -0400

    refactored meta base meta scraprs

---
+++ rob/scrapr/bawebcn.py
@@ -0,0 +1,24 @@
+
+from urllib2 import urlopen
+from urllib2 import URLError
+from urllib2 import Request
+from lxml.html import fromstring
+
+class BACnt(object):
+  domain = 'http://www.beeradvocate.com'
+  
+  def __init__(self):
+    self.connect = None
+  
+  def brewer_meta(self, brewer_id):
+    url = 'http://www.beeradvocate.com/beer/profile/%i' % brewer_id
+    return self.__open(url)
+    
+  def beer_meta(self, brewer_id, beer_id):
+    url = 'http://www.beeradvocate.com/beer/profile/%i/%i' % (brewer_id, beer_id)
+    return self.__open(url)
+    
+  def __open(self, url):
+    req = Request(url, headers = {'User-Agent': 'Mozilla'})
+    return urlopen(req)
+    
