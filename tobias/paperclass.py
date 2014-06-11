ec862a9077bbc5c31a709c61efcf53f26b1bc24d
tobiasewilson <tobias.e.wilson@gmail.om>
2014-06-11 15:35:24 -0400

    a script that creates a python class Paper

---
+++ tobias/paperclass.py
@@ -0,0 +1,22 @@
+testfile=open("/home/tobias/BigHarvest/oai:arXiv.org:0704.0001.arXiv.xml","r")
+from bs4 import BeautifulSoup
+souptest = BeautifulSoup(testfile)    
+class Paper:
+  def __init__(self, paperfile):
+    for i in range(0,len(keyname)):
+      namelist.append((keyname[i].text, forename[i].text))        
+    self.title = paperfile.select('title')[0].text
+    self.ident = paperfile.select('id')[0].text
+    self.authors = namelist
+    self.abstract = paperfile.select('abstract')[0].text
+    self.categories = paperfile.select('categories')[0].text 
+    self.create = paperfile.select('created')[0].text
+    
+    
+x = Paper(souptest)
+#print x.title
+#print x.ident
+#print x.authors
+#print x.abstract
+print x.categories
+print x.create
