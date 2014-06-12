fb8170972e78743fc0bf48b14a38eaa5b4114693
tobiasewilson <tobias.e.wilson@gmail.om>
2014-06-12 16:18:05 -0400

    edited scripts for reading files into db

--- tobias/PaperList.py
+++ tobias/PaperList.py
@@ -1,11 +1,24 @@
-# This script reads a file, list.txt, that lists all the files in a directory. It then opens each file with BeautifulSoup and saves them in a 
-# list, PaperList. THis is useful for me right now because I'm working with a small directory. I need a different solution when I work with 
-# actual directory of xml files; I don't want to put all 10^6 in a single list. 
+# This script reads a file, list.txt, that lists all
+# the papers in a directory. It then opens each file
+# one by one, creates a temporary Paper object, b,
+# and adds its summary to a database. 
+import sqlite3 as lite
+import sys
+con = lite.connect('test.db')
+with con:
+  cur = con.cursor()
+  cur.execute("DROP TABLE IF EXISTS Papertest")
+  cur.execute("CREATE TABLE Papertest(Id TEXT, Created TEXT, Title TEXT")
+
+
 temp = open("/home/tobias/Harvests/list.txt","r")
 directory = temp.read().splitlines()
-PaperList=[]
+directory.pop(0) #first line is list.txt
 for line in directory:
     a = open("/home/tobias/Harvests/"+line,"r")
-    PaperList.append(BeautifulSoup(a))
-PaperList.pop(0) # at least in this example, the list file is the first file in Harvests, but I don't want it in the PaperList
+    b = Paper(BeautifulSoup(a))
+    with con:
+      cur = con.cursor()
+      cur.execute("INSERT INTO Papertest VALUES(?,?,?)", b.summary)
+
 
