ec9e4dc442f0a4961da3f2ec2abf2d7d1c68a392
Andrew Schechtman-Rook <rook166@gmail.com>
2014-06-17 18:45:38 -0400

    web frontend and simple backend done, visualization working

--- andrew/delayr_site/delayr/urls.py
+++ andrew/delayr_site/delayr/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
                        url(r'^$',views.index,name='index'),
                        url(r'^model_output$',views.model_output,name='model_output'),
                        url(r'^test$',views.test,name='test'),
+                       url(r'^user_prediction_(?P<string_prediction>.+).jpg$$',views.show_user_prediction),
                        )
--- andrew/delayr_site/delayr/views.py
+++ andrew/delayr_site/delayr/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse
+
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.ticker import ScalarFormatter
+import pickle as pickle
 
 from delayr.models import Airlinenames,FlightdelaysAprAfternoon,Airports
 from delayr.forms import AirportForm,AirlineForm,DateTimeForm
@@ -35,6 +41,7 @@ def index(request):
                 prediction_values = list(prediction_df.irow(0))
                 prediction = zip(prediction_bins,prediction_values)
                 context_dict['prediction'] = prediction
+                context_dict['string_prediction'] = repr(prediction)
             #airlineform = AirlineForm(initial={'uniquecarrier':valuedict['uniquecarrier']})
             #print 'test',airlineform.fields['uniquecarrier'].initial
             print valuedict['uniquecarrier']
@@ -68,3 +75,39 @@ def model_output(request):
     return index(request)
     context = RequestContext(request)
 
+
+def show_user_prediction(request,string_prediction):
+    context = RequestContext(request)
+    prediction = eval(string_prediction)
+    unzipped_prediction = zip(*prediction)
+    delay_bins = unzipped_prediction[0]
+    delay_likelihood = np.array(unzipped_prediction[1])
+    x_pos = np.arange(len(delay_bins))
+    f = plt.figure()
+    f.set_facecolor('none')
+    ax = f.add_subplot(111)
+    width = 1.0
+    #ax.plot(delay_likelihood*100,delay_likelihood*100,ls='.',marker='o',mec='red',mfc='red',alpha=0.5)
+    #print x_pos,delay_likelihood*100
+    logbool = False
+    if logbool:
+        ax.set_yscale("log")
+        ax.bar(x_pos,delay_likelihood*100.,width,color='red',alpha=0.5,log=True)
+    else:
+        ax.bar(x_pos,delay_likelihood*100.,width,color='red',alpha=0.5)
+    ax.set_ylim(ax.get_ylim()[0],100)
+    ax.set_title("Predicted Delay for Selected Itinerary")
+    ax.set_ylabel("% Probability")
+    ax.set_xlabel("Delay (minutes)")
+    ax.set_xticks(x_pos+width/2.)
+    ax.set_xticklabels(delay_bins)
+    ax.yaxis.set_major_formatter(ScalarFormatter())
+    
+    # x = np.random.normal(loc=0,scale=1.0,size=100)
+    # y = np.random.normal(loc=0,scale=1.0,size=100)
+    # ax.plot(x,y,ls=".",marker="o",ms=2,mec='red',mfc='red',alpha=0.5)
+    canvas = FigureCanvasAgg(f)
+    response = HttpResponse(content_type='image/png')
+    canvas.print_png(response,bbox_inches='tight')
+    plt.close(f)
+    return response
