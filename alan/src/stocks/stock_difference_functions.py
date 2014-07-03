a37d8ad94d2d395124cf0e292d59e16c158c6a3d
Alan Meert <alan.meert@gmail.com>
2014-07-03 13:49:00 -0400

    final code addition

---
+++ alan/src/stocks/stock_difference_functions.py
@@ -0,0 +1,193 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime 
+import pandas as pd
+import scipy.signal as signal
+import statsmodels.api as sm
+import pickle
+import pandas.io.sql as psql
+import MySQLdb as mysql
+import scipy as sp
+import sklearn.linear_model
+import sklearn.tree
+import sklearn.metrics
+from statsmodels.graphics.api import qqplot
+
+import sys
+sys.path.append('../')
+import settings
+from utils import *
+
+from stock_returns import load_stock, date_contract
+
+
+# Reporting function
+def summarize_errors(test_me, real, model):
+    error_pct = (test_me[real] -test_me[model])/test_me[real]
+
+    print error_pct.describe()
+    error_pct.plot()
+    plt.ylim(-0.5, 0.5)
+    plt.show()
+
+    error_pct.hist(range= (-0.5, 0.5),bins=1000, normed=True)
+#, weights = np.ones_like(error_pct)/error_pct.size)
+    x = np.arange(-0.5, 0.5, 0.001)
+    plt.plot(x, sp.stats.norm( loc=error_pct.median(), scale=(error_pct.quantile(0.84)-error_pct.quantile(0.16))/2.0 ).pdf(x), linewidth=3, color='red')
+    plt.xlim(-0.5,0.5)
+    plt.show()
+
+    print "your model %.2e" %sklearn.metrics.mean_squared_error( test_me[real].fillna(0), test_me[model].fillna(0) )
+    print "simple %.2e" %sklearn.metrics.mean_squared_error( test_me[real].fillna(0),test_me[real].shift(1).fillna(0))
+    return
+
+def error_figure(stock_data, resid_key, predict_key, norm_predict,figtitle):
+    fig = plt.figure(figsize=(12,8))
+    plt.subplots_adjust(hspace=0.6, bottom=0.1)
+    plt.figtext(0.5, 0.97,figtitle)
+
+    ax = fig.add_subplot(221)
+    stock_data[resid_key].plot()
+    plt.xlabel('Date')
+    plt.ylabel('Absolute Residual Volume')
+    plt.title('Absolute Residual Volume')
+
+    ax = fig.add_subplot(222)
+    stock_data['volume'].plot(label='volume')
+    stock_data[predict_key].plot(label='predicted volume')
+    plt.xlabel('Date')
+    plt.ylabel('Volume')
+    plt.title('Predicted and True Volume')
+    plt.legend()
+
+    ax = fig.add_subplot(223)
+    stock_data[norm_predict].hist(range = (-0.75, 0.75), bins=100, 
+                                  normed=True)
+    x = np.arange(-0.75, 0.75, 0.001)
+    #trim some crazy outliers
+    trimmed_resid = np.extract(np.logical_and(stock_data[norm_predict]<1,
+                                              stock_data[ norm_predict]>-1),
+                               stock_data[norm_predict])  
+    plt.plot(x, sp.stats.norm(loc=trimmed_resid.mean(), 
+                              scale=trimmed_resid.std()).pdf(x), 
+             linewidth=3, color='red')
+    plt.xlim(-0.75,0.75)
+    plt.title('normed residual distribution')
+    plt.xlabel('Date')
+    plt.ylabel('Volume')
+
+    ax = fig.add_subplot(224)
+    plt.title('folded residual')
+    plt.scatter(stock_data['time_remaining'],
+                stock_data[resid_key]/stock_data[predict_key], s=2)
+    plt.subplots_adjust(hspace=0.4, wspace=0.4)
+    plt.ylabel('residual relative to prediction')
+    plt.xlabel('fraction remaining of contract')
+    print stock_data[resid_key].describe()
+    return
+
+def normed_avg(df, key, span = 50, min_periods = 3):
+    """takes the log of the value at lag 1 relative to the Exp. weighted MA"""
+    value = np.log(df[key].shift(1)/pd.stats.moments.ewma(df[key].shift(1), span=span, min_periods=min_periods)).fillna(method='backfill').fillna(method='ffill')
+    return value
+
+def economic_predictors(df):
+    """GDP, CPI, and other simple predictors"""
+    df['percapGDP'] = df['GDP']/df['Population']
+    df['normGDP'] =normed_avg(df, 'percapGDP')
+
+    df['normFedFunds'] =normed_avg(df, 'FedFundsRate')
+    return df
+
+def gdelt_predictors(df):
+    """event predictors. We normalize by the moving average to account for event count inflation"""
+    for country in ['US', 'IR', 'RS', 'NO','KS','SA', 'NI','JA','GM','FR']:
+        df['normcount%s' %country] = normed_avg(df, 'count%s' %country)
+        for event in [18,19]:
+            df['normcount%d%s' %(event,country)] = normed_avg(df,'count%d%s' %(event,country))
+    return df
+
+
+def price_predictors(df):
+    df['log_ret'] = np.log(df['settle']/df['settle'].shift(1))
+    df['log_ret_yesterday'] = df['log_ret'].shift(1).fillna(method='backfill')
+
+    df['log_ret'] = df['log_ret'].fillna(value=0)
+
+    df['ewma_ratio'] = normed_avg(df, 'settle', span = 5, min_periods = 3)
+    df['ewma50_ratio']= normed_avg(df, 'settle', span = 50, min_periods = 3)
+
+    df = economic_predictors(df)
+
+    df = gdelt_predictors(df)
+    
+    df['const'] = np.ones_like(df['settle'].values)
+
+    return df
+
+
+def volume_predictors(df):
+    df['log_vol'] = np.log(df['volume']/df['volume'].shift(1))
+    df['log_yesterday'] = df['log_vol'].shift(1).fillna(method='backfill')
+
+    df['log_vol'] = df['log_vol'].fillna(value=0)
+    df['ewma_ratio'] = normed_avg(df, 'volume', span = 5, min_periods = 3)
+    df['ewma50_ratio']= normed_avg(df, 'volume', span = 50, min_periods = 3)
+
+
+    df = economic_predictors(df)
+    df = gdelt_predictors(df)
+    df['const'] = np.ones_like(np.array(df['settle'].values))
+    return df
+
+
+def get_pred_arr(df):
+    cols_to_use = ['ewma50_ratio','ewma_ratio','log_yesterday',
+                   'um_sent','time_remaining', 'normFedFunds',
+                   'normGDP','stress_ind', 'normcountUS', 'normcountIR', 
+                   'normcountRS', 'normcountNO','normcountKS',
+                   'normcountSA', 'normcountNI',
+                   'normcountJA','normcountGM','normcountFR',
+                   'normcount19US', 'normcount19IR', 
+                   'normcount19RS', 'normcount19NO','normcount19KS',
+                   'normcount19SA', 'normcount19NI',
+                   'normcount19JA','normcount19GM','normcount19FR',
+                   'normcount18US', 'normcount18IR', 
+                   'normcount18RS', 'normcount18NO','normcount18KS',
+                   'normcount18SA', 'normcount18NI',
+                   'normcount18JA','normcount18GM','normcount18FR', 'const']
+    outarr = np.array([df[a] for a in cols_to_use]).T 
+    return outarr
+
+def plot_error(true, prediction, title, scorer):
+    fig = plt.figure()
+    plt.scatter(true, true-prediction, s=2)
+    plt.title(title)
+    plt.ylabel('error (true-predict)')
+    plt.xlabel('true change')
+    plt.text(.1,.1,'score:%.2e' %scorer(true,prediction), transform=plt.gca().transAxes)
+    return
+
+def plot_vol_error(true, prediction, title, scorer):
+    fig = plt.figure()
+    plt.scatter(true, (true-prediction)/true, s=2)
+    plt.title(title)
+    plt.ylabel('error (true-predict)/true')
+    plt.xlabel('true volume')
+    plt.xlim((20000.0,plt.xlim()[1]))
+    plt.ylim((-1.5,1.5))
+    plt.text(.1,.1,'score:%.2e' %scorer(true,prediction), transform=plt.gca().transAxes)
+    return
+
+def class_error(true, prediction, title, scorer):
+    fig = plt.figure()
+    imgplot = plt.imshow(sklearn.metrics.confusion_matrix(true, prediction)/float(true.size), origin='bottom')
+    imgplot.set_interpolation('nearest')
+    plt.colorbar()
+    plt.title(title)
+    plt.ylabel('actual class')
+    plt.xlabel('predicted class')
+    plt.text(-.1,-.1,'score:%.2e' %scorer(true,prediction), transform=plt.gca().transAxes)
+
+    return
+    
---
+++ alan/src/stocks/volume_difference_regress.py
@@ -0,0 +1,204 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime 
+import pandas as pd
+import scipy.signal as signal
+import statsmodels.api as sm
+import pickle
+import pandas.io.sql as psql
+import MySQLdb as mysql
+import scipy as sp
+import sklearn.linear_model
+import sklearn.tree
+import sklearn.metrics
+from statsmodels.graphics.api import qqplot
+from sklearn import preprocessing
+
+
+import sys
+sys.path.append('../')
+import settings
+from utils import *
+
+from stock_returns import load_stock, date_contract
+
+Conn = mysql.connect(host = settings.host,
+                     user = settings.user,
+                     db = settings.db)
+Conn.autocommit(True)
+cursor = Conn.cursor()
+
+from stock_difference_functions import *
+
+from sklearn.metrics import mean_squared_error
+scorer = mean_squared_error
+
+if __name__ == "__main__":
+    comm_choice = settings.comm_choice
+    maxdays=settings.maxdays
+    train_start_date = do_date(settings.train_start_date)
+    train_end_date = do_date(settings.train_end_date)
+
+    test_start_date = do_date(settings.test_start_date)
+    test_end_date = do_date(settings.test_end_date)
+
+    
+    #load the data 
+    stock_data = load_stock(comm_choice, train_start_date, train_end_date)
+    test_data = load_stock(comm_choice, test_start_date, test_end_date)
+
+    # calculate the log return, which is simpler to model
+    stock_data = volume_predictors(stock_data)
+    test_data = volume_predictors(test_data)
+    
+    fig = plt.figure(figsize=(12,8))
+    plt.subplots_adjust(hspace=0.4)
+    ax1 = fig.add_subplot(211)
+    plt.title('Volume Autocorrelation')
+    plt.xlabel('Lag (Business Days)')
+    plt.ylabel('Correlation')
+    sm.graphics.tsa.plot_acf(stock_data['volume'].values.squeeze(), lags=60, ax=ax1)
+    ax2 = fig.add_subplot(212)
+    plt.title('Volume PAcf')
+    plt.xlabel('Lag (Business Days)')
+    plt.ylabel('Correlation')
+    sm.graphics.tsa.plot_pacf(stock_data['volume'], lags=60, ax=ax2)
+    plt.figtext(0.5, 0.95,'Volume Correllations')
+    plt.savefig('volume_corr.png')
+
+
+    arma_mod11 = sm.tsa.ARMA(stock_data['volume'], (1,1)).fit()
+    print arma_mod11.summary()
+    stock_data['ARMAPredictVolume'] = arma_mod11.predict()
+    stock_data['ARMAResid'] = arma_mod11.resid
+    stock_data['ARMANormResids'] =arma_mod11.resid/stock_data['volume'].shift(1)
+    stock_data['ARMANormResids']= stock_data['ARMANormResids'].fillna(0)
+
+
+    arma_test11 = sm.tsa.ARMA(test_data['volume'], (1,1)).fit()
+    print arma_test11.summary()
+    test_data['ARMAPredictVolume'] = arma_test11.predict()
+    test_data['ARMAResid'] = arma_test11.resid
+    test_data['ARMANormResids'] =arma_test11.resid/test_data['volume'].shift(1)
+    test_data['ARMANormResids']= test_data['ARMANormResids'].fillna(0)
+    
+
+    fig = plt.figure(figsize=(12,8))
+    plt.subplots_adjust(hspace=0.4)
+    ax1 = fig.add_subplot(211)
+    plt.xlabel('Lag (Business Days)')
+    plt.ylabel('Correlation')
+    plt.title('Residual Volume Autocorrelation')
+    sm.graphics.tsa.plot_acf(stock_data['ARMAResid'].values.squeeze(), lags=60, ax=ax1)
+    ax2 = fig.add_subplot(212)
+    plt.title('Residual Volume  Pacf')
+    plt.xlabel('Lag (Business Days)')
+    plt.ylabel('Correlation')
+    sm.graphics.tsa.plot_pacf(stock_data['ARMAResid'], lags=60, ax=ax2)
+    plt.savefig('volume_ARMAresidcorr.png')
+    
+    fig = plt.figure(figsize=(12,8))
+    plt.title('qq plot of the ARMA Volume Residual')
+    qqplot(stock_data['ARMAResid'], line='q', ax=plt.gca(), fit=True)
+    plt.savefig('volARMAresid_qq.png')
+
+    error_figure(stock_data, 'ARMAResid', 'ARMAPredictVolume', 
+                 'ARMANormResids','ARMA Only')
+    plt.savefig('ARMA_stock_residual_panel.png')
+
+    error_figure(test_data, 'ARMAResid', 'ARMAPredictVolume', 
+                 'ARMANormResids','ARMA Only')
+    plt.savefig('ARMA_test_residual_panel.png')
+    plt.close('all')
+    
+    train_arr = get_pred_arr(stock_data)
+    test_arr =get_pred_arr(test_data)     
+
+    scaler = preprocessing.StandardScaler().fit(train_arr)
+    sctrain_arr = scaler.transform(train_arr) 
+    sctest_arr = scaler.transform(test_arr) 
+
+    #there is some remaining autocorrelation at ~20 days. This is due to the monthly roll-off, so lets fit a decision tree to the folded data.
+    print "Decision Tree"
+    from sklearn import tree
+    clf = tree.DecisionTreeRegressor()
+    stock_data['rolloff'] = clf.fit(np.array(stock_data['time_remaining'])[:,np.newaxis],stock_data['ARMAResid']/stock_data['ARMAPredictVolume']).predict(np.array(stock_data['time_remaining'])[:,np.newaxis])*stock_data['ARMAPredictVolume']
+    test_data['rolloff'] = clf.predict(np.array(test_data['time_remaining'])[:,np.newaxis])*test_data['ARMAPredictVolume']
+    
+    def rolloff(df):
+        df['timePredictVolume'] = (df['ARMAPredictVolume']+df['rolloff']).fillna(-999).fillna(method='ffill')
+        df['timeResid'] = (df['volume'].values-df['timePredictVolume']).fillna(-999).fillna(-999)
+        df['timeNormResids'] =df['timeResid']/df['volume'].shift(1)
+        df['timeNormResids']= df['timeNormResids'].fillna(-999).fillna(-999)
+        return df
+
+    stock_data = rolloff(stock_data)
+    test_data = rolloff(test_data)
+
+    error_figure(test_data, 'timeResid', 'timePredictVolume', 
+                 'timeNormResids','roll-off plus ARMA')
+    plt.savefig('time_test_residual_panel.png')
+
+
+    plot_vol_error(test_data['volume'].values,test_data['timePredictVolume'], 'decision tree regression test set', scorer)
+    plt.xlabel('volume')
+    plot_vol_error(test_data['volume'].values,test_data['ARMAPredictVolume'], 'test set ARMA only', scorer)
+    plot_vol_error(test_data['volume'].values,test_data['timePredictVolume'], 'test set time resid corrected', scorer)
+    plt.xlabel('volume')
+    plt.savefig('test_rolloff_residual_panel.png')
+
+    from sklearn.cross_validation import train_test_split
+    volume_train, volume_cv, true_train, true_cv = train_test_split(sctrain_arr, stock_data['log_vol'].fillna(method='backfill').values, test_size=0.33, random_state=42)
+
+    from sklearn import dummy
+    dumreg = dummy.DummyRegressor()
+    dumreg.fit(volume_train, true_train)
+    dumguess = dumreg.predict(sctest_arr)
+    plot_error(test_data['log_vol'].values, dumguess, 'Dumb regression test set', scorer)
+    plt.savefig('volcorr_Dummy.png')
+    
+    clf = sklearn.linear_model.LinearRegression()
+    clf.fit(volume_train, true_train)
+    stock_data['predictchange']=clf.predict(sctrain_arr)
+    plot_error(test_data['log_vol'].values, clf.predict(sctest_arr), 'linear regression test set', scorer)
+    plt.savefig('volcorr_linear.png')
+
+    print "Decision Tree"
+    from sklearn import tree
+    clf = tree.DecisionTreeRegressor()
+    clf = clf.fit(volume_train, true_train).predict(sctest_arr)
+    plot_error(test_data['log_vol'].values,clf, 'decision tree regression test set', scorer)
+    plt.savefig('volcorr_DecTree.png')
+
+    param_grid=[ {'n_estimators':range(5,56,10), 
+                  'min_samples_split':range(5,36,10),
+                  'min_samples_leaf':range(10,51,10)},]
+
+    print "Random Forest"
+    from sklearn.ensemble import RandomForestClassifier
+    clf = RandomForestClassifier(n_estimators=10)
+    clf = sklearn.grid_search.GridSearchCV(clf, param_grid, scoring='mean_squared_error', verbose = 10)
+    clf.fit(volume_train, true_train)
+    clf = clf.predict(sctest_arr)
+    plot_error(test_data['log_vol'].values,clf, 'random forest regression test set', scorer)
+    plt.savefig('volcorr_RandFor.png')
+
+    print "SVM"
+    from sklearn import svm
+    param_grid = [
+        {'C': [0.001,0.01,0.1, 1.0], 'kernel': ['linear']},
+        {'C': [0.001,0.01,0.1,1.0], 'gamma': [0.01, 0.001], 
+         'kernel': ['rbf']},
+        ]
+    clf = svm.SVR()
+    clf = sklearn.grid_search.GridSearchCV(clf, param_grid, scoring='mean_squared_error', verbose = 10)
+    clf.fit(volume_train, true_train)
+    plot_error(test_data['log_vol'].values,clf.predict(sctest_arr), 'SVM regression test set', scorer)
+    print clf.get_params()
+    plt.savefig('volcorr_SVM.png')
+
+
+
+
+
+
